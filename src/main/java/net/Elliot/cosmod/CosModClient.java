6aa418c1722d0d2882517118cc4238454dafd5a3
ellifire123 <46790716+ellifire123@users.noreply.github.com>
2023-12-27 00:06:07 +0100

    Initial commit

---
+++ src/main/java/net/Elliot/cosmod/CosModClient.java
@@ -0,0 +1,10 @@
+package net.Elliot.cosmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CosModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
